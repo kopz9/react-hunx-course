c863303b1c0c4ecb50f37eba2019e809b9044bba
kopz9 <gustavoeduardo438@gmail.com>
2024-02-08 23:04:52 -0300

    asd

--- counter/src/App.js
+++ counter/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Re
+          Learn React \\\\
         </a>
       </header>
     </div>
