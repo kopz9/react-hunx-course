18894c76fcbcfabeca1606fa86a572aba5874880
kopz9 <gustavoeduardo438@gmail.com>
2024-02-11 17:06:23 -0300

    feat: validate

---
+++ counter/src/utils/content.js
@@ -0,0 +1,20 @@
+export const accordionData = [
+  {
+    title: "What is HTML?",
+    content: `The HyperText Markup Languuage or HTML is the standard markup language for documents designed to be displayed in a web browser. 
+    It can be assisted by techonologies such as Cascading Style Sheets and scripting languages such as JavaScript.`,
+  },
+  {
+    title: "What is React?",
+    content: `React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies`,
+  },
+  {
+    title: "What is Node.js?",
+    content: `Node.js is a cross-platform, open-source server environment that can run on Windows,
+    Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime enviroment, runs on the V8 JavaScript Engine, and executes Javascript code outside a web browser.`
+  },
+  {
+    title: "What is Golang?",
+    content: `Go is a statically typed, compiled programming language designed at Google by Robert Griesemer, Rob Pike, and Ken Thompson. It is syntactically similar to C, but with memory safety, garbage collection, structural typing, and CSP-style concurrency`
+  }
+]
